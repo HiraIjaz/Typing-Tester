a9615d2285f57fa5fbd30ab2d62728ef94a4ff8b
Hira <hiraijaz956@gmail.com>
2023-08-09 18:44:54 +0500

    :sparkles:Implemented Typing Tester

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
+import TypingTester
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    data = TypingTester.fetch_data()[0]
+    TypingTester.typing_tester(data['word'])
+    print('\n')
+    print("Definition: ", data['definition'])
+    print("Pronunciation: ", data['pronunciation'])
